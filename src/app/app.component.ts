4d76a0af873e3ffc1b1eec25344625ece58a5af4
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-01-18 20:55:45 +0200

    app.component points to login.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button'; // import Button Module
+import { ButtonModule } from 'primeng/button';
+import {LoginComponent} from './login/login.component'; // import Button Module
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule], // Add Button Module to imports
+  imports: [ButtonModule, LoginComponent], // Add Button Module to imports
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

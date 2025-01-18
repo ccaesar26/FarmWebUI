acab874439554185a79bb97b72d132be4c331b19
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-01-18 23:25:01 +0200

    Login UI successfully created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+
+import {Checkbox} from 'primeng/checkbox';
+import {InputText} from 'primeng/inputtext';
+import {Button} from 'primeng/button';
+import {FormsModule} from '@angular/forms';
+import {Password} from 'primeng/password';
+import {FloatLabel} from 'primeng/floatlabel';
+import {Fluid} from 'primeng/fluid';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Checkbox,
+    InputText,
+    FormsModule,
+    Password,
+    Button,
+    FloatLabel,
+    Fluid,
+    ReactiveFormsModule,
+    InputIconModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = new FormControl('');
+  password = new FormControl('');
+  rememberMe: boolean = false;
 
 }

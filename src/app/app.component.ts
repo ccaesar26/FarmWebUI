00e5aff67d11d70f41d7df7b64ba6b20fec52396
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-01-21 17:49:18 +0200

    Before Adding Mapbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LandingComponent} from './landing/landing.component'; // import Button Module
 
 @Component({
   selector: 'app-root',
   imports: [
-    LandingComponent,
     RouterOutlet
   ], // Add Button Module to imports
   templateUrl: './app.component.html',

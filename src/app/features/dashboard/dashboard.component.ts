c53e9ded908c8303499bb68a576bdc36c6da69e0
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-02-22 21:53:05 +0200

    Update dashboard.component.ts

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
-import { NgForOf } from '@angular/common';
 import { PrimeTemplate } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     Card,
-    NgForOf,
     PrimeTemplate
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  cards = Array.from({ length: 144 }, (_, i) => i + 1); // 12 rows × 12 columns = 144 cards
 }
